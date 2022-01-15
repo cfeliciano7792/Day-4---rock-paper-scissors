8de0d3ba8aa9ad5acd1e520ce465a9c55643a4fc
Christian F <96458958+cfeliciano7792@users.noreply.github.com>
2022-01-15 13:58:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+import random
+user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n")
+
+pc_choice = random.randint(0,2)
